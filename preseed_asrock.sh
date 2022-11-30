98203a64a29c39929d9f5a5933ef3edf7e81144d
Barnabas Busa <busa.barnabas@gmail.com>
2022-11-30 11:48:32 +0100

    update

--- preseed_asrock.sh
+++ preseed_asrock.sh
@@ -15,9 +15,9 @@ d-i debian-installer/locale string en_US
 d-i keyboard-configuration/xkb-keymap select us
 # d-i keyboard-configuration/toggle select No toggling
 
-# Load missing firmware from removable media?
-d-i	hw-detect/load_firmware	boolean	false
-d-i	hw-detect/load_media	boolean	false
+# # Load missing firmware from removable media?
+# d-i	hw-detect/load_firmware	boolean	false
+# d-i	hw-detect/load_media	boolean	false
 
 ### Network configuration
 # Disable network configuration entirely. This is useful for cdrom
@@ -30,7 +30,7 @@ d-i	hw-detect/load_media	boolean	false
 #d-i netcfg/choose_interface select auto
 
 # To pick a particular interface instead
-d-i netcfg/choose_interface select enp1s0f0: Realtek Semiconductor Co.\, Ltd. RTL8111/8168/8411 PCI Express Gigabit Ether
+d-i netcfg/choose_interface select enp1s0f0: Realtek Semiconductor Co., Ltd. RTL8111/8168/8411 PCI Express Gigabit Ether
 
 # To set a different link detection timeout (default is 3 seconds).
 # Values are interpreted as seconds.
@@ -72,8 +72,8 @@ d-i netcfg/dhcpv6_timeout string 60
 # Any hostname and domain names assigned from dhcp take precedence over
 # values set here. However, setting the values still prevents the questions
 # from being shown, even if values come from dhcp.
-d-i netcfg/get_hostname seen true
-d-i netcfg/get_domain seen true
+d-i netcfg/get_hostname string asrock-debian
+d-i netcfg/get_domain string testing.com
 
 # If you want to force a hostname, regardless of what either the DHCP
 # server returns or what the reverse DNS entry for the IP is, uncomment
